59aada5ea2b69f8a951f64c16a82b1e5c8d47228
kushkumar80 <113597756+kushkumar80@users.noreply.github.com>
2025-11-19 13:07:31 +0530

    Add comment in SelfController for Jenkins test
    Added a comment for Jenkins test clarification.

--- src/main/java/com/nit/springboot_jenkinns/SelfController.java
+++ src/main/java/com/nit/springboot_jenkinns/SelfController.java
@@ -5,7 +5,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class SelfController {
-
+// this the jenkins test 
     @RequestMapping("/test")
     public String test(){
         return "This is the testing of jenkins";
